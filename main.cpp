f04d98cf1b688e0b25e180ca2ee8e22854870e28
Exfell <100291975+Exfell@users.noreply.github.com>
2024-02-05 14:32:16 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <string>
 
+void EraseNullsFromEnd(std::string& dec, int point) {
+	for (int i = dec.size()-1; i > point;i--)
+	{
+		if (dec[i] == '0') {
+			dec.erase(i, 1);
+		}
+		else { break; }
+	}
+}
+
 int main() {
 
 	std::string decimal{};
@@ -10,32 +20,50 @@ int main() {
 	std::cout << "Write floating point number ";
 	std::getline(std::cin, decimal);
 	//Determining the index of a point
-	for (int i = 0; i < decimal.size(); i++)
+	point = decimal.find('.');
+	if (!(point>0)) { point = decimal.size(); }
+
+	//проверка начала
+	int tempPoint = point;
+	for (int i = 0; i < tempPoint;)
 	{
-		if (decimal[i] == '.') point = i;
+		if (decimal[i] != '0') { exp = tempPoint - 1 - i; break; }
+		else { 
+			decimal.erase(i, 1);
+			tempPoint--;
+		}
 	}
-	//Calculating the number of characters that will leave the mantissa and go to the exponent
-	exp = point - 1;
-	std::string newDecimal = decimal;
-	newDecimal[1] = '.';
-	float isPoint = false;
-	for (int i = 1; i < decimal.size(); i++)
-	{
-		if (i != point) {
-			if (!isPoint) {
-				newDecimal[i + 1] = decimal[i];
-			}
-			else {
-				newDecimal[i] = decimal[i];
-			}
+	//Если число с 0 в целой части
+	if (exp == 0) {
+		point = 0;
+		for (int i = point; i < decimal.size() - 1;)
+		{
+			if ((decimal[i] != '0') and (decimal[i]!='.')) { break; }
+			exp--;
+			decimal.erase(i, 1);
+		}
+	}
+	std::string newDecimal;
+	//СЦЕНАРИЙ, ПРИ КОТОРОМ ЭКСПОНЕНТА НЕОТРИЦАТЕЛЬНАЯ
+	if (exp >= 0) {
+		decimal.erase(point, 1);
+		std::string rest = decimal.substr(1, decimal.size() - 1);
+		//Делаем 2345123
+		EraseNullsFromEnd(rest, 0);
+		//Если число как меньше 10, то у него остальная часть равна пустой строке
+		if (rest == "") {
+			newDecimal = (decimal[0] +('e' + std::to_string(exp)));
 		}
 		else {
-			isPoint = true;
+			newDecimal = (decimal[0] + ('.' + rest + 'e') + std::to_string(exp));
 		}
 	}
-	std::string exponent{ 'e' + std::to_string(exp) };
-	newDecimal += exponent;
+
+	//СЦЕНАРИЙ, ПРИ КОТОРОМ ЭКСПОНЕНТА ОТРИЦАТЕЛЬНАЯ
+	if (exp < 0) {
+		std::string rest = decimal.substr(1, decimal.size() - 1);
+		newDecimal = (decimal[0] + ('.' + rest + 'e') + std::to_string(exp));
+
+	}
 	std::cout << newDecimal;
-	// input: 12341.131231231
-	// output: 1.2341131231231e4
 }
